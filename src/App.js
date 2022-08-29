3ee3e85fe104298419d3da660e50bba8c4d7180d
Ravinder <sspl.ravinder2021@gmail.com>
2022-08-29 12:10:40 +0530

    irst commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
+
+import * as tf from '@tensorflow/tfjs'
+import * as speechCommands from '@tensorflow-models/speech-commands';
+
 
 function App() {
+ useEffect(()=>{
+async function myfunc(){
+
+  try{
+  const recognizer = speechCommands.create('BROWSER_FFT');
+
+  await recognizer.ensureModelLoaded();
+
+// See the array of words that the recognizer is trained to recognize.
+console.log(recognizer.wordLabels());
+
+// `listen()` takes two arguments:
+// 1. A callback function that is invoked anytime a word is recognized.
+// 2. A configuration object with adjustable fields such a
+//    - includeSpectrogram
+//    - probabilityThreshold
+//    - includeEmbedding
+recognizer.listen(result => {
+  // - result.scores contains the probability scores that correspond to
+  //   recognizer.wordLabels().
+  // - result.spectrogram contains the spectrogram of the recognized word.
+}, {
+  includeSpectrogram: true,
+  probabilityThreshold: 0.75
+});
+  }
+  catch(e){
+    console.log(e);
+  }
+}
+myfunc();
+
+
+ })
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/audiofacility.js
@@ -0,0 +1,10 @@
+
+// When calling `create()`, you must provide the type of the audio input.
+// The two available options are `BROWSER_FFT` and `SOFT_FFT`.
+// - BROWSER_FFT uses the browser's native Fourier transform.
+// - SOFT_FFT uses JavaScript implementations of Fourier transform
+//   (not implemented yet).
+
+
+// Make sure that the underlying model and metadata are loaded via HTTPS
+// requests.
